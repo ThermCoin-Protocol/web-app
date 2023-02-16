776c5a453c7c50e9d29ecded1835a51d3431718b
CarterBloop <carter@tiogaventures.com>
2023-02-16 13:06:25 -0800

    Style changes

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React from 'react';
 
 export default function Hero() {
   return (
-    <div className="hero min-h-screen bg-secondary">
-      <div className="hero-content text-center">
-        <div className="max-w-lg">
-          <h1 className="text-4xl font-bold">A digital currency backed by energy</h1>
+    // Background image set to bg.jpg
+    <div className="hero min-h-screen bg-[url('./bg.jpg')]">
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="text-4xl font-bold">Energy-based Digital Currency</h1>
           <p className="py-6 text-lg">
-            ThermCoin solves problems related to energy
-            economics and tackles our future of energy and transactions.
-            We are a community of people who are passionate about fair and
-            decentralized solutions to the problems of our time.
+            ThermCoin solves problems related to energy economics and tackles
+            our future of energy and transactions. We are a community of people
+            who are passionate about fair and decentralized solutions to the
+            problems of our time.
           </p>
-          <button className="btn btn-accent">Start Mining</button>
+          <button className="btn-accent btn">Start Mining</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,44 +1,68 @@
-import React from 'react'
+import React from 'react';
 
 const THEMES = [
-  "cupcake",
-  "bumblebee",
-  "corporate",
-  "synthwave",
-  "retro",
-  "cyberpunk",
-  "lofi",
-  "pastel",
-  "fantasy",
-  "cmyk",
-  "autumn",
-  "business",
-  "coffee",
-  "winter",
-]
+  'cupcake',
+  'bumblebee',
+  'corporate',
+  'synthwave',
+  'retro',
+  'cyberpunk',
+  'lofi',
+  'pastel',
+  'fantasy',
+  'cmyk',
+  'autumn',
+  'business',
+  'coffee',
+  'winter',
+  'night',
+];
 
 export default function Navbar() {
-    
   return (
-    <div className="navbar bg-info fixed z-20">
+    <div className="navbar fixed z-20 bg-neutral">
       <div className="navbar-start">
         <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost btn-circle">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+          <label tabIndex={0} className="btn-ghost btn-circle btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
           </label>
-          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a href='https://github.com/ThermCoin-Protocol'>GitHub</a></li>
-            <li><a href='https://discord.gg/evAR9ng8WR'>Discord</a></li>
-            <li><a>Primer</a></li>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
+          >
+            <li>
+              <a href="https://github.com/ThermCoin-Protocol">GitHub</a>
+            </li>
+            <li>
+              <a href="https://discord.gg/evAR9ng8WR">Discord</a>
+            </li>
+            <li>
+              <a>Primer</a>
+            </li>
           </ul>
         </div>
       </div>
       <div className="navbar-center">
-        <a className="btn btn-ghost normal-case text-4xl text-nuetral">Therm Coin</a>
+        <a className="btn-ghost btn text-4xl normal-case text-neutral-content">
+          ThermCoin
+        </a>
       </div>
       <div className="navbar-end invisible sm:visible">
-        <select className="select select-bordered w-2/5">
-          <option defaultValue='light' selected>
+        <select className="select-bordered select w-2/5">
+          <option defaultValue="light" selected>
             theme
           </option>
           {THEMES.map((themeName) => (
@@ -47,6 +71,5 @@ export default function Navbar() {
         </select>
       </div>
     </div>
-  )
+  );
 }
-
