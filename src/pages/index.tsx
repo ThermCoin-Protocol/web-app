8f31ce9fbda559e1594ae19e401572d220a6cbc2
Cody Lambert <codylambertdev@gmail.com>
2023-02-25 17:10:59 -0800

    Remove unused var

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { useTheme } from 'next-themes';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
 import Hero from '@/components/hero';
-import Hook from '@/components/hook';
+// import Hook from '@/components/hook';
 // import Action from '@/components/action';
 import { useEffect } from 'react';
 import Stats from '@/components/stats';
