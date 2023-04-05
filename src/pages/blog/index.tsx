fa365870699c3f1db8bfcb251dcd766013a6ce66
Cody Lambert <codylambertdev@gmail.com>
2023-04-05 13:54:50 -0700

    blog cards

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -61,29 +61,8 @@ const BlogPage: NextPageWithLayout<BlogProps> = ({ posts, categories }: BlogProp
 
   return (
     <div className="flex flex-col text-gray-800 mx-10 lg:mx-20 md:mx-20">
-      {/* <h1 className="text-7xl text-left mb-10">Blog</h1> */}
-      {/* <div className="self-center xl:w-3/5 cursor-pointer h-fit content-center mb-10 border border-black rounded-3xl p-10">
-        <Link href={`/blog/${posts[0].slug.current}`}>
-          <div>
-            <div className="flex flex-row justify-between">
-              <h2 className="text-3xl text-left">{posts[0].title}</h2>
-              <p className="text-lg text-left">{posts[0].publishedAt}</p>
-            </div>
-            <div className="flex flex-row">
-              {posts[0].categories.map((category: Category, index: number) => (
-                <p key={index} className="text-lg text-left">{category.title}</p>
-              ))}
-            </div>
-            <p className="text-lg text-left">{posts[0].publishedAt}</p>
-            
-          <Image src={urlFor(posts[0].mainImage).url()} width={1600} height={1000}  
-            className="absolute w-full object-cover"          
-          />
-          </div>
-        </Link>
-      </div> */}
       <div className="grid lg:grid-cols-5">
-        <div className=" border-b border-black lg:border-base-100 col-span-5 lg:col-span-1 sticky top-0 lg:top-20 z-10 bg-base-100 py-5 mb-5 lg:py-0 h-fit">
+        <div className="border-b border-gray-800 lg:border-base-100 col-span-5 lg:col-span-1 sticky top-0 lg:top-20 z-10 bg-base-100 py-5 mb-5 lg:py-0 h-fit">
           <h2 className="text-3xl text-left">Categories</h2>
           <CategorySelect categories={categories} handleCategoryChange={handleCategoryChange} />
         </div>
@@ -91,7 +70,7 @@ const BlogPage: NextPageWithLayout<BlogProps> = ({ posts, categories }: BlogProp
           {filteredPosts.map((post, index) => (
             <div key={index} className="cursor-pointer h-fit">
               <Link href={`/blog/${post.slug.current}`}>
-                <div key={post._id} className="flex flex-col w-full border border-black rounded-3xl p-10 h-80 hover:bg-[#F5F5F5]">
+                <div key={post._id} className="flex flex-col w-full border border-gray-800 rounded-3xl p-10 h-80 hover:bg-[#F5F5F5] group">
                   {/* <Image src={urlFor(post.mainImage).url()} width={1600} height={1000}  
                     className="absolute w-full object-cover"          
                   /> */}
@@ -100,10 +79,31 @@ const BlogPage: NextPageWithLayout<BlogProps> = ({ posts, categories }: BlogProp
                     <h2 className="text-xl">{post.title}</h2>
                     <p className="text-lg text-left">{new Date(post.publishedAt).toLocaleDateString()}</p>
                   </div>
-                  <div className="border-b border-black w-full my-5">
+                  <div className="border-b border-gray-800 w-full my-5">
                     {post.categories.map((category: Category, index: number) => (
                       <p key={index} className="text-lg text-left">{category.title}</p>
                     ))}
+                  </div>
+                  <div className="flex-grow flex place-items-center">
+                    {post.body.map((block: any, index: number) => {
+                        if (block._type === "block" && index === 0) {
+                          return (
+                            <p key={index} className="text-lg text-left">
+                              {block.children[0].text}
+                            </p>
+                          );
+                        }
+                      })
+                    }
+                  </div>
+                  <div className="flex justify-end">
+                    <a href="#_" className="relative inline-flex items-center px-10 py-3 overflow-hidden text-lg font-medium text-gray-800 border border-gray-800 rounded-full">
+                      <span className="absolute left-0 block w-full h-0 transition-all bg-indigo-600 opacity-100 hover:h-full top-1/2 duration-400 ease"></span>
+                      <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-2 ease">
+                      <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                      </span>
+                      <span className="relative">Read More</span>
+                    </a>
                   </div>
                 </div>
               </Link>
@@ -130,7 +130,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
       },
       alt
     },
-    publishedAt
+    publishedAt,
+    body
   }`;
   const categoryQuery = '*[_type == "category"]{title}';
   const posts = await sanityClient.fetch(query);
