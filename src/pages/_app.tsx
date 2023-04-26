4794af35a6fa0fce649fe095673ea7a371ee95f5
Cody Lambert <codylambertdev@gmail.com>
2023-04-25 17:20:37 -0700

    vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import type { ComponentType, ReactElement, ReactNode } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import '@/styles/index.css';
 
 export type NextPageWithLayout<P = {}> = NextPage<P> & {
@@ -16,5 +17,10 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>,
+  );
 }
