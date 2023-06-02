3a460d79e069ecaba84adaee2d7cd0943f907c4b
Cody Lambert <codylambertdev@gmail.com>
2023-06-02 12:24:12 -0700

    git it working, now I am checking headers

--- src/pages/api/sendDiscordWebhookMessage.ts
+++ src/pages/api/sendDiscordWebhookMessage.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import {isValidSignature, SIGNATURE_HEADER_NAME} from '@sanity/webhook';
+import {isValidSignature, SIGNATURE_HEADER_NAME} from '@sanity/webhook'
 // I am trying to make an API endpoint that will send a message to a discord webhook
 // I have a webhook setup with sanity and discord but I need to make an API endpoint that will send a message to the webhook
 export default async function sendDiscordWebhookMessage(_req: NextApiRequest, res: NextApiResponse) {
@@ -15,27 +15,11 @@ export default async function sendDiscordWebhookMessage(_req: NextApiRequest, re
   } else {
     res.status(200).json({success: true, message: 'Valid signature'})
   }
-  // get the signature from the request headers
-  // get the body from the request
-  // get the event type from the request headers
-  const eventType = _req.headers['x-sanity-event-type'] || '';
-  // get the document id from the request headers
-  const documentId = _req.headers['x-sanity-document-id'] || '';
-  // get the dataset from the request headers
-  const dataset = _req.headers['x-sanity-dataset'] || '';
-  // get the project id from the request headers
-  const projectId = _req.headers['x-sanity-project-id'] || '';
-  // get the timestamp from the request headers
-  const timestamp = _req.headers['x-sanity-timestamp'] || '';
 
   // log the request details
   console.log('signature', signature);
   console.log('body', body);
-  console.log('eventType', eventType);
-  console.log('documentId', documentId);
-  console.log('dataset', dataset);
-  console.log('projectId', projectId);
-  console.log('timestamp', timestamp);
+  console.log('header', _req.headers);
 
 
   // get the webhook url from the environment variables
